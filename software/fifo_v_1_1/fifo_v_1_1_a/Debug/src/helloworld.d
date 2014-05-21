2517cc74e8c8253b017b977ee22f595cef6e1777
Muhammad Aqeel <ga97guz@TUEISEC-Pra04.(none)>
2014-05-21 20:49:57 +0200

    new commit after fifo_v_1_1

---
+++ software/fifo_v_1_1/fifo_v_1_1_a/Debug/src/helloworld.d
@@ -0,0 +1,42 @@
+src/helloworld.d: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h \
+ ../../standalone_bsp_0/microblaze_0/include/xio.h \
+ ../../standalone_bsp_0/microblaze_0/include/xbasic_types.h \
+ ../../standalone_bsp_0/microblaze_0/include/xintc_l.h \
+ ../../standalone_bsp_0/microblaze_0/include/xil_types.h \
+ ../../standalone_bsp_0/microblaze_0/include/xil_assert.h \
+ ../../standalone_bsp_0/microblaze_0/include/xparameters.h \
+ ../../standalone_bsp_0/microblaze_0/include/xil_io.h \
+ ../../standalone_bsp_0/microblaze_0/include/mb_interface.h \
+ ../../standalone_bsp_0/microblaze_0/include/xil_printf.h \
+ ../../standalone_bsp_0/microblaze_0/include/xparameters.h \
+ ../../standalone_bsp_0/microblaze_0/include/sc_uart.h \
+ ../../standalone_bsp_0/microblaze_0/include/xstatus.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../../standalone_bsp_0/microblaze_0/include/xio.h:
+
+../../standalone_bsp_0/microblaze_0/include/xbasic_types.h:
+
+../../standalone_bsp_0/microblaze_0/include/xintc_l.h:
+
+../../standalone_bsp_0/microblaze_0/include/xil_types.h:
+
+../../standalone_bsp_0/microblaze_0/include/xil_assert.h:
+
+../../standalone_bsp_0/microblaze_0/include/xparameters.h:
+
+../../standalone_bsp_0/microblaze_0/include/xil_io.h:
+
+../../standalone_bsp_0/microblaze_0/include/mb_interface.h:
+
+../../standalone_bsp_0/microblaze_0/include/xil_printf.h:
+
+../../standalone_bsp_0/microblaze_0/include/xparameters.h:
+
+../../standalone_bsp_0/microblaze_0/include/sc_uart.h:
+
+../../standalone_bsp_0/microblaze_0/include/xstatus.h:
